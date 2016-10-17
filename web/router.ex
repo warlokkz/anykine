734ed5893e339b9b3443aaf07ca63ca8bc518540
Alexander Anich <alexander.anich@gmail.com>
2016-10-17 10:08:24 -1000

    introduce react, apollo & have graphql also working

--- web/router.ex
+++ web/router.ex
@@ -1,8 +1,23 @@
 defmodule Anykine.Router do
   use Anykine.Web, :router
 
-  get "/graphiql", Absinthe.Plug.GraphiQL, schema: Anykine.Schema
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
 
-  forward "/", Absinthe.Plug,
-    schema: Anykine.Schema
+  get "/graphiql", Absinthe.Plug.GraphiQL, schema: Anykine.Schema
+  post "/graphiql", Absinthe.Plug.GraphiQL, schema: Anykine.Schema
+
+  forward "/graphql", Absinthe.Plug,
+  schema: Anykine.Schema
+
+  scope "/" , Anykine do
+    pipe_through :browser
+
+    get "/", PageController, :index
+  end
 end
